a625063ce2b5777f99bfd1eb1b91ba10b9bd876b
Hasan Hüseyin GÜNEŞ <hgunes@RPK.LOCAL>
2021-08-26 14:03:44 +0300

    net5mailservice

---
+++ MailService_WebApi/MailService_WebApi/Controllers/MailController.cs
@@ -0,0 +1,40 @@
+﻿using MailService_WebApi.Models;
+using MailService_WebApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailService_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+        public MailController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult>SendMail([FromForm]MailRequest request )
+        {
+            try
+            {
+                await _mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+
+
+    }
+}
---
+++ MailService_WebApi/MailService_WebApi/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using MailService_WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailService_WebApi.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
